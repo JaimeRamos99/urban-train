68ff76792785f301769b018d6854c4fd1d40d307
Jaime Ramos <jrossetes@gmail.com>
2021-12-05 17:03:39 -0500

    chore: change date format in joi schema

--- src/adapters/requestValidatorJoi/order.ts
+++ src/adapters/requestValidatorJoi/order.ts
@@ -1,8 +1,11 @@
 import Joi from 'joi';
+import joiDate from '@joi/date';
+
+Joi.extend(joiDate);
 
 const orderJoiSchema = Joi.object({
     id: Joi.string().required(),
-    date: Joi.date().required(),
+    date: Joi.date().raw().required(),
     quantity: Joi.number().required(),
     productID: Joi.string().required(),
     productName: Joi.string().required(),
