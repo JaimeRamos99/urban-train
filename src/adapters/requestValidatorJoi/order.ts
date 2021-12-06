3c28ff69e0f5e0e17009af4e3b94afadb9642e7e
Jaime Ramos <jrossetes@gmail.com>
2021-12-05 19:59:19 -0500

    fix: add correct format for date attribute in validation schema

--- src/adapters/requestValidatorJoi/order.ts
+++ src/adapters/requestValidatorJoi/order.ts
@@ -1,11 +1,12 @@
-import Joi from 'joi';
+import baseJoi from 'joi';
 import joiDate from '@joi/date';
+import { constants } from '../../common/constants';
 
-Joi.extend(joiDate);
+const Joi = baseJoi.extend(joiDate);
 
 const orderJoiSchema = Joi.object({
     id: Joi.string().required(),
-    date: Joi.date().raw().required(),
+    date: Joi.date().format(constants.time.dateFormat).raw().required(),
     quantity: Joi.number().required(),
     productID: Joi.string().required(),
     productName: Joi.string().required(),
